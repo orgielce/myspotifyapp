1f97a2308c3bfd909b88fde443b75234be341c67
Orgiel CE <orgielce@gmail.com>
2022-07-05 16:26:36 -0400

    fix (pagination) Adjust pagination

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -8,31 +8,38 @@ export const Pagination = () => {
   const dispatch = useDispatch();
   const [page, setPage] = useState(useSelector((state: RootState) => state.token.page));
 
-  const setNextValue = () => {
+  const setNextValue = (e: any) => {
+    e.preventDefault();
     setPage(page + 10);
     dispatch(setCurrentPage({page}));
   };
 
-  const setPreviousValue = () => {
+  const setPreviousValue = (e: any) => {
+    e.preventDefault();
     if (page === 10) return;
+    dispatch(setCurrentPage({page: page - 10}));
     setPage(page - 10);
-    dispatch(setCurrentPage({page}));
   };
 
   return (
     <div className="flex flex-nowrap mt-8 py-3 text-3xl cursor-pointer ml-auto md:mr-10 lg:mr-10 2xl:mr-28">
-      {page === 10 && (
-        <div onClick={setPreviousValue} className="mr-5 opacity-25">
-          <BsArrowLeftCircle />
-        </div>
-      )}
-      {page > 10 && (
-        <div onClick={setNextValue} className="mr-5">
-          <BsArrowLeftCircle />
-        </div>
-      )}
+      {/*{page === 10 && (*/}
+      {/*  <div onClick={setPreviousValue} className="mr-5 opacity-25">*/}
+      {/*    <BsArrowLeftCircle />*/}
+      {/*  </div>*/}
+      {/*)}*/}
+      {/*{page > 10 && (*/}
+      {/*  <div onClick={setPreviousValue} className="mr-5">*/}
+      {/*    <BsArrowLeftCircle />*/}
+      {/*  </div>*/}
+      {/*)}*/}
+      <div onClick={setPreviousValue} className="mr-5">
+        {page === 10 && <BsArrowLeftCircle className="opacity-25" />}
+        {page > 10 && <BsArrowLeftCircle />}
+      </div>
+
       <div onClick={setNextValue}>
-        <BsArrowRightCircle></BsArrowRightCircle>
+        <BsArrowRightCircle />
       </div>
     </div>
   );
